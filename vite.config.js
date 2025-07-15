b3d6b3e3cff045aede39438836bfeec733c822d4
Jorge Bastori <jorgebastori@gmail.com>
2025-07-15 23:35:16 +0200

    Limpieza final de referencias a submódulos rotos para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+<<<<<<< HEAD
+=======
+  base: '/Planetario-VR-/', // Necesario para GitHub Pages
+>>>>>>> 25d8136 (Limpieza final de referencias a submódulos rotos para GitHub Pages)
   server: {
     port: 5173,
     host: '0.0.0.0',
